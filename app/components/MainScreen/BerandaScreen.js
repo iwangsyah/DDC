984a8179a61f6494bb12e5030d4dbe69124d8fde
Wahyu Irwansyah <wahyuiwangsyah@gmail.com>
2018-11-06 14:19:50 +0700

    add font

---
+++ app/components/MainScreen/BerandaScreen.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import styles from '../../styles/MainScreen/beranda'
+
+export default class Beranda extends Component<Props> {
+  render() {
+    
+    return (
+      <View style={styles.container}>
+
+      </View>
+    );
+  }
+}
---
+++ app/lib/images.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import AkunActive from '../assets/icons/akun_ic_active';
+import AkunInactive from '../assets/icons/akun_ic_inactive';
+import HomeActive from '../assets/icons/home_ic_active';
+import HomeInactive from '../assets/icons/home_ic_inactive';
+import KatalogActive from '../assets/icons/katalog_ic_active';
+import KatalogInactive from '../assets/icons/katalog_ic_inactive';
+import KomunitasActive from '../assets/icons/komunitas_ic_active';
+import KomunitasInactive from '../assets/icons/komunitas_ic_inactive';
+import PembayaranActive from '../assets/icons/pembayaran_ic_active';
+import PembayaranInactive from '../assets/icons/pembayaran_ic_inactive';
+
+export class AkunActiveIcon extends Component {
+  render () {
+    return (
+      <AkunActive/>
+    )
+  }
+}
+
+export class AkunInactiveIcon extends Component {
+  render () {
+    return (
+      <AkunInactive/>
+    )
+  }
+}
+
+export class HomeActiveIcon extends Component {
+  render () {
+    return (
+      <HomeActive/>
+    )
+  }
+}
+
+export class HomeInactiveIcon extends Component {
+  render () {
+    return (
+      <HomeInactive/>
+    )
+  }
+}
+
+export class KatalogActiveIcon extends Component {
+  render () {
+    return (
+      <KatalogActive/>
+    )
+  }
+}
+
+export class KatalogInactiveIcon extends Component {
+  render () {
+    return (
+      <KatalogInactive/>
+    )
+  }
+}
+
+export class KomunitasActiveIcon extends Component {
+  render () {
+    return (
+      <KomunitasActive/>
+    )
+  }
+}
+
+export class KomunitasInactiveIcon extends Component {
+  render () {
+    return (
+      <KomunitasInactive/>
+    )
+  }
+}
+
+export class PembayaranActiveIcon extends Component {
+  render () {
+    return (
+      <PembayaranActive/>
+    )
+  }
+}
+
+export class PembayaranInactiveIcon extends Component {
+  render () {
+    return (
+      <PembayaranInactive/>
+    )
+  }
+}
